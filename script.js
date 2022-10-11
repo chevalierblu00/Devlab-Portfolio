2941268df4e53e52f856609c27c09116d40c457d
Martin <mgaspard.perso@gmail.com>
2022-10-11 16:21:41 +0200

    :lipstick: :sparkles: interface finaliser plus poppup

---
+++ script.js
@@ -0,0 +1,5 @@
+let bloc_1 = document.querySelector(".bloc_1");
+let popup_container = document.querySelector(".popup_container");
+bloc_1.addEventListener("click", function() {
+    popup_container.classList.toggle("active")
+})
---
+++ script2.js
@@ -0,0 +1,5 @@
+let bloc_2 = document.querySelector(".bloc_2");
+let popup_container2 = document.querySelector(".popup_container2");
+bloc_2.addEventListener("click", function() {
+    popup_container2.classList.toggle("active")
+})
