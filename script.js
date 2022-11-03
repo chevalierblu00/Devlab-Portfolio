c9bdfcce3d7f92c70cf77890e68dd17bee17cba6
chevalierblu00 <113120254+chevalierblu00@users.noreply.github.com>
2022-11-03 15:24:40 +0100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-let bloc_1 = document.querySelector(".bloc_1");
-let popup_container = document.querySelector(".popup_container");
-bloc_1.addEventListener("click", function() {
-    popup_container.classList.toggle("active")
-})
