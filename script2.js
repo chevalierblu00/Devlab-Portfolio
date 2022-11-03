2fd94fabe4485e5e01d09f8692e3c02429e3b01f
Martin <mgaspard.perso@gmail.com>
2022-11-03 14:59:59 +0100

    refonte

--- script2.js
+++
@@ -1,5 +0,0 @@
-let bloc_2 = document.querySelector(".bloc_2");
-let popup_container2 = document.querySelector(".popup_container2");
-bloc_2.addEventListener("click", function() {
-    popup_container2.classList.toggle("active")
-})
