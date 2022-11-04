582146c03ea1e4ec678a2288e9e66ce93c1b424b
Martin <mgaspard.perso@gmail.com>
2022-11-04 08:59:57 +0100

    udpate

--- script.js
+++
@@ -1,6 +0,0 @@
-let video_minature = document.querySelector(".video_minature");
-let popup_container = document.querySelector(".popup_container");
-video_minature.addEventListener("click", function() {
-    popup_container.classList.toggle("active")
-})
-
