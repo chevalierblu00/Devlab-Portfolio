85ee25580c6a73a434635f798f7f393afe2322d6
Martin <mgaspard.perso@gmail.com>
2023-07-09 18:44:56 +0200

    modification

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-$(window).scroll(function(){
-  var scrolledFromTop = $(window).scrollTop() + $(window).height();
-  $(".appear").each(function(){
-    var distanceFromTop = $(this).offset().top;
-    if(scrolledFromTop >= distanceFromTop+100){
-      var delaiAnim = $(this).data("delai");
-      $(this).delay(delaiAnim).animate({
-        top:0,
-        opacity:1
-      });
-    }
-  });
+var isFirstLoad = true;
+
+$(window).scroll(function() {
+  if (!isFirstLoad) {
+    var scrolledFromTop = $(window).scrollTop() + $(window).height();
+    $(".appear").each(function() {
+      var distanceFromTop = $(this).offset().top;
+      if (scrolledFromTop >= distanceFromTop + 100) {
+        var delaiAnim = $(this).data("delai");
+        $(this).delay(delaiAnim).animate({
+          top: 0,
+          opacity: 1
+        });
+      }
+    });
+  }
 });
+
+// Après la première exécution du code, définir isFirstLoad sur false
+isFirstLoad = false;
